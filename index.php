917c70fc391e8de585ef344bde34562daf0cbf9b
Darkk820 <ivan.alex65@gmail.com>
2018-07-06 15:21:39 -0500

    Inicio

--- index.php
+++ index.php
@@ -1,10 +1,13 @@
 <!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <title>Document</title>
-</head>
-<body>
-    <h1>Hello</h1>
-</body>
+<html lang="es">
+
+    <head>
+        <meta charset="UTF-8">
+        <title>Helhetkode</title>
+    </head>
+
+    <body>
+        
+    </body>
+
 </html>
