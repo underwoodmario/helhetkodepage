fc9bf51ab0966c5064eac370775ff024129cc9f8
Mario Underwood <underwoodmarioarturo@gmail.com>
2019-03-27 20:28:15 -0600

    Cleanup

--- php/formu.php
+++
@@ -1,7 +0,0 @@
-<?php
-    define("MYSQL_CONN_ERROR", "Unable to connect to database.");
-    mysqli_report(MYSQLI_REPORT_STRICT || MYSQLI_REPORT_ERROR || MYSQLI_REPORT_ALL);
-
-    hostinguer
-    $mysqli = mysqli_connect("sql145.main-hosting.eu.", "u165839073_usuar", "54gHvGzDnJuoO8Ho12", "u165839073_pagina");
-?>
